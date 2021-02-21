84c9fcc0d80b99ceec63feccce92775a954193e9
John Grese <johngrese@yahoo.com>
2021-02-21 09:07:50 -0800

    pypi config updates

--- setup.py
+++ setup.py
@@ -1,30 +1,25 @@
-import setuptools
-from pathlib import Path
+from setuptools import setup, find_packages
+import pathlib
 
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-version = '0.0.1'
-
-setuptools.setup(
-    name='maraboupy-neuralnetworkverification', # Replace with your own username
-    version=version,
-    author='Marabou',
-    author_email='',
-    description='Marabou Neural Network Verification Framework for Python',
+setup(
+    name='maraboupy-NeuralNetworkVerification',
+    version='0.0.0',
+    description='Marabou Neural Network Verification Framework Python Interface',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    # url='https://github.com/NeuralNetworkVerification/Marabou',
-    url='https://github.com/grese/Marabou',
-    packages=setuptools.find_packages(),
+    url='https://github.com/NeuralNetworkVerification/Marabou',
+    author='Marabou',
     classifiers=[
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
-        'License :: Modified BSD License',
-        'Operating System :: Linux',
-        'Operating System :: Mac OS',
-        # 'Operating System :: Windows'
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
     ],
+    keywords='neural network, formal verification',
+    packages=find_packages(),
     python_requires='>=3.5',
-    install_requires=['pybind11 >= 2.3.0'],
-    dependency_links=[]
+    install_requires=['pybind11 >=2.3.0'],
 )
