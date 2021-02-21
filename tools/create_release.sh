41e236a384732d2424b79e9277cbfa6123e6ee8b
John Grese <johngrese@yahoo.com>
2021-02-21 09:10:27 -0800

    updated build configurations

--- tools/create_release.sh
+++ tools/create_release.sh
@@ -4,12 +4,13 @@ RELEASE_VERSION="$1"
 
 echo "copying release assets to ./release"
 mkdir release
-cp -R ./dist/Linux/g++/maraboupy setup.py COPYING README.md ./release/
+cp -R ./dist/Linux/g++/maraboupy MANIFEST.in pyproject.toml setup.cfg setup.py COPYING README.md ./release/
 mkdir ./release/maraboupy/bin
 cp ./dist/Linux/g++/Marabou ./release/maraboupy/bin/Marabou-linux
 
 echo "removing unnecessary files from maraboupy package"
 cd ./release/maraboupy/ && rm -rf test __pycache__ .pytest_cache && cd -
 
-echo "updating version in setup.py"
+echo "updating version in setup.py & setup.cfg"
 perl -pe "s/version = '(\d+\.)*\d+'/version = '$RELEASE_VERSION'/" -i ./release/setup.py
+perl -pe "s/version = (\d+\.)*\d+/version = $RELEASE_VERSION/" -i ./release/setup.cfg
