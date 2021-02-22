27b7706bb0963e0d43ecfd2f3fddf54296b55bdf
John Grese <johngrese@yahoo.com>
2021-02-22 01:33:04 -0800

    publish job pip install and list release dir

--- tools/create_release.sh
+++ tools/create_release.sh
@@ -4,13 +4,14 @@ RELEASE_VERSION="$1"
 
 echo "copying release assets to ./release"
 mkdir release
-cp -R ./dist/Linux/g++/maraboupy MANIFEST.in pyproject.toml setup.cfg setup.py COPYING README.md ./release/
+cp -R ./dist/Linux/g++/maraboupy MANIFEST.in pyproject.toml setup.cfg setup.py README.md ./release/
 cp ./release/COPYING ./release/LICENSE
 mkdir ./release/maraboupy/bin
 cp ./dist/Linux/g++/Marabou ./release/maraboupy/bin/Marabou-linux
+# cp ./dist/MacOS/g++/Marabou ./release/maraboupy/
 
 echo "removing unnecessary files from maraboupy package"
-cd ./release/maraboupy/ && rm -rf test __pycache__ .pytest_cache && cd -
+cd ./release/maraboupy/ && rm -rf test __pycache__ .pytest_cache test_requirements.txt && cd -
 
 echo "updating version in setup.py & setup.cfg"
 perl -pe "s/version = '(\d+\.)*\d+'/version = '$RELEASE_VERSION'/" -i ./release/setup.py
